0b167a4a83e6fb9cb40f5c4bd28138c6734c8787
Name <email@example.ru>
2024-08-21 23:10:44 +0700

    Модль 3. Конец раздела 3.2

--- Modules/Module3.BaseC/Program.cs
+++ Modules/Module3.BaseC/Program.cs
@@ -13,6 +13,15 @@
             Console.WriteLine("\u0040");
             Console.WriteLine("\u0023");
 
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            Console.WriteLine(5);
+
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
+
             Console.ReadKey();
         }
     }
