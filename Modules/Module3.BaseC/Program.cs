a59d977373c9117c3fa2f05a5014c2a36aa1dbb1
Name <email@example.ru>
2024-08-21 23:07:41 +0700

    Модуль 3. Задание 3.2.8

--- Modules/Module3.BaseC/Program.cs
+++ Modules/Module3.BaseC/Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "NewSuperJane";
+            const string MyName = "Jane";
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
 
             Console.ReadKey();
         }
