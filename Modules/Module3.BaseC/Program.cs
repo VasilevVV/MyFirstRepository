7874c3d93d6d55849657c0330b6e0218e4c3656c
Name <email@example.ru>
2024-09-02 20:38:42 +0700

    Модуль 3. Задание 3.6.1.

--- Modules/Module3.BaseC/Program.cs
+++ Modules/Module3.BaseC/Program.cs
@@ -4,28 +4,20 @@
     {
         static void Main(string[] args)
         {
-            string MyName = "Евгения";
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine();
-            Console.WriteLine("Меня зовут {0}", MyName);
+            double result = 10 / 4;
+            Console.WriteLine("10 / 4 = {0}", result);
 
-            //0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+            double result2 = 14 % 7;
+            Console.WriteLine("14 % 7 = {0}", result2);
 
+            double result3 = 14 / 7;
+            Console.WriteLine("14 / 7 = {0}", result3);
 
-            //Также мы можем вставить в строку не переменную, а строковый литерал:
-            Console.WriteLine("Меня зовут {0}", "Маша");
+            double result4 = 14.0 / 7.0;
+            Console.WriteLine("14.0 / 7.0 = {0}", result4);
 
-
-            //И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
-            var age = 27;
-            var weight = 50;
-            
-            Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
-
-            string favcolor = "black";
-            string name = "Jane";
-            Console.WriteLine("{0}\n  {1} \n {2}", name, age, favcolor);
-
+            double result5 = 10.0 / 4.0;
+            Console.WriteLine("10.0 / 4.0 = {0}", result5);
 
             Console.ReadKey();
         }
