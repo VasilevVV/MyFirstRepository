4b024e17f75d25f98cedf4d86295ef5ca6a1304b
Name <email@example.ru>
2024-09-05 20:54:28 +0700

    Модуль 4. Задание 4.3.11

--- Modules/Module3.BaseC/Program.cs
+++ Modules/Module3.BaseC/Program.cs
@@ -4,99 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-            Console.WriteLine("Цикл while");
-            int k = 0;
-            while (k < 1)
-            {                
-                Console.WriteLine("Iteration {0}", k);
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                switch (Console.ReadLine())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                    Console.Write(array[i, k] + " ");
 
-                        Console.WriteLine("Your color is red!");
-
-                        Console.ResetColor();
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-
-                        Console.ResetColor();
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-
-                        Console.ResetColor();
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is yellow!");
-
-                        Console.ResetColor();
-                        break;
-                }
-                k++;
+                Console.WriteLine();
             }
 
-            Console.WriteLine("Цикл do");
-            int t = 0;
-            do
+            Console.WriteLine();
+
+            for (int i = 0; i <= array.GetUpperBound(1); i++)
             {
-                Console.WriteLine("Iteration {0}", t);
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                switch (Console.ReadLine())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
+                for (int k = 0; k <= array.GetUpperBound(0); k++)
+                    Console.Write(array[k, i] + " ");
 
-                        Console.WriteLine("Your color is red!");
-
-                        Console.ResetColor();
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-
-                        Console.ResetColor();
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-
-                        Console.ResetColor();
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is yellow!");
-
-                        Console.ResetColor();
-                        break;
-                }
-                t++;
-            } while (t < 1);
+                Console.WriteLine();
+            }
 
             Console.ReadKey();
         }
