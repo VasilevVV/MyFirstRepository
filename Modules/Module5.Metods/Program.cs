7ddf831efd8655b609040bcf0e207dd1101d806d
Name <email@example.ru>
2024-09-26 13:14:13 +0700

    Модуль 5. Конец раздела 5.2.

--- Modules/Module5.Metods/Program.cs
+++ Modules/Module5.Metods/Program.cs
@@ -1,10 +1,122 @@
-﻿namespace Module5.Metods
+﻿using System.Reflection;
+
+namespace Module5.Metods
 {
     internal class Program
     {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            Console.ResetColor();
+            return color;
+        }
+
+        static void ShowColors(string username = "Jane", params string[] favcolors)
+        {
+            Console.WriteLine($"{username}\nВаши любимые цвета:");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+
+        static int[] GetArrayFromConsole(int num = 5)
+        {
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            /*
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            */
+
+            return result;
+        }
+
+        static int[] SortArray(int[] result)
+        {
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+
+        static void ShowArray(int[] arr, bool isSort = false)
+        {
+            var temp = arr;
+            if (isSort = true)
+            {
+                temp = SortArray(arr);
+            }
+
+            foreach (var item in temp)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("");
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            var favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length;i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            ShowColors(favcolors[0], favcolors[2]);
+            */
+
+            var array = GetArrayFromConsole(10);
+            ShowArray(array, true);
+
+
+            Console.ReadKey();
         }
     }
 }
