7ee64c7ac2db68f487fca779b9ee7cdbf7662825
Name <email@example.ru>
2024-10-09 20:41:54 +0700

    Модуль 6. Задание 6.3.1.

--- Modules/Module6.Classes/Program.cs
+++ Modules/Module6.Classes/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var department = GetCurrentDepartment();
+
+            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            {
+                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+            }
+
+            Console.ReadKey();
+        }
+
+        static Department GetCurrentDepartment()
+        {
+            // logic 
         }
     }
+
+    class Company
+    {
+        public string Type;
+        public string Name;
+    }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+
 }
