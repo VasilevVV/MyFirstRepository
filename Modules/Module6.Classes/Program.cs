473484a57e488053516d859f123c2b9b7a1fc0f7
Name <email@example.ru>
2024-10-09 20:45:08 +0700

    Модуль 6. Задание 6.3.2.

--- Modules/Module6.Classes/Program.cs
+++ Modules/Module6.Classes/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Module6.Classes
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -18,6 +18,24 @@
         {
             // logic 
         }
+    }
+
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue && Load > 0)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+
     }
 
     class Company
